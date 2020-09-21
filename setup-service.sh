579d5276fdf64587386c4625d41c83641a6d8b17
TJ Biegner <tbiegner99@gmail.com>
2020-09-20 22:16:59 -0400

    modify infra scripts

--- setup-service.sh
+++ setup-service.sh
@@ -2,7 +2,7 @@
 
 path=`dirname ${BASH_SOURCE[0]}`
 
-cp $path/temperature.service /etc/systemd/systemd
+cp $path/temperature.service /etc/systemd/system
 
 systemctl enable temperature
 systemctl start temperature
