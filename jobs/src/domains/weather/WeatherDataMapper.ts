4d4891a8d1e421db7fb1455402e46fed761b2f30
TJ Biegner <tbiegner99@gmail.com>
2025-11-11 23:23:44 -0500

    debug

--- jobs/src/domains/weather/WeatherDataMapper.ts
+++ jobs/src/domains/weather/WeatherDataMapper.ts
@@ -81,7 +81,7 @@ export class WeatherDataMapper {
       };
       readings.push(reading);
     }
-
+    console.log(readings);
     return {
       stationId: 44065,
       stationName: 'Breezy Point, NY',
